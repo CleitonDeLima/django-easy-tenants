d905b919d0c1f4687359025d953f1c9bb6193c57
Cleiton Lima <cleiton.limapin@gmail.com>
2020-03-14 01:25:58 -0300

    Added the tenant_context function and update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+import pytest
 from django.conf import settings
+
+from easy_tenants import tenant_context, get_tenant_model
 
 
 def pytest_configure():
@@ -22,3 +25,10 @@ def pytest_configure():
         ],
         TENANT_MODEL='tests.StoreTenant'
     )
+
+
+@pytest.fixture
+def context(db):
+    tenant = get_tenant_model().objects.create()
+    with tenant_context(tenant):
+        yield
--- tests/test_easy_tenants.py
+++ tests/test_easy_tenants.py
@@ -5,7 +5,8 @@ from tests.models import StoreTenant
 from easy_tenants import (
     get_tenant_model,
     set_current_tenant,
-    get_current_tenant
+    get_current_tenant,
+    tenant_context
 )
 
 
@@ -23,3 +24,15 @@ def test_get_and_set_current_tenant_thread_local():
     set_current_tenant(tenant)
 
     assert tenant == get_current_tenant()
+
+
+@pytest.mark.django_db
+def test_tenant_context():
+    tenant1 = StoreTenant.objects.create()
+    tenant2 = StoreTenant.objects.create()
+    set_current_tenant(tenant1)
+
+    with tenant_context(tenant2):
+        assert tenant2 == get_current_tenant()
+
+    assert tenant1 == get_current_tenant()
