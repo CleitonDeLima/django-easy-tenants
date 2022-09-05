20d1ea7ecad6bceefa9986a7b7ac8e88bcc8163c
Cleiton Lima <cleiton.limapin@gmail.com>
2022-09-05 13:51:16 -0300

    Change easytenants model

--- easy_tenants/models.py
+++ easy_tenants/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 from django.db.models.expressions import BaseExpression
-from django.db.models.functions import Cast
 
 from easy_tenants import get_current_tenant
 from easy_tenants.utils import get_state
 
 
 class CurrentTenant(BaseExpression):
+    def __init__(self, field_name, output_field=None):
+        # TODO: Usar o field_name para pegar o campo a ser filtrado no estado salvo
+        self.field_name = field_name
+        super().__init__(output_field)
+
     def as_sql(self, compiler, connection, *args, **kwargs):
         current_tenant = get_current_tenant()
         tenant_id = str(current_tenant.id)
         value = self.output_field.get_db_prep_value(tenant_id, connection)
-        return "%s", [str(value)]
+
+        # TODO: (%s) é usado com o lookup in, portanto se tiver mais de um tenant no filtro é pra usar assim
+        return "(%s)", [str(value)]
 
 
 class TenantManager(models.Manager):
@@ -23,8 +29,10 @@ class TenantManager(models.Manager):
             return queryset
 
         field = self.model.tenant.field.target_field
-        cr = CurrentTenant(output_field=field)
-        queryset = queryset.filter(tenant_id=Cast(cr, output_field=field))
+        queryset = queryset.filter(
+            tenant_id=CurrentTenant("tenant", output_field=field)
+        )
+
         return queryset
 
     def bulk_create(self, objs):
